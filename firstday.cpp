0ced2556e49dc4eb1566dd4f826fa9b754a75554
GregoryAMiller <32182450+GregoryAMiller@users.noreply.github.com>
2019-02-07 12:49:33 -0800

    Update firstday.cpp

--- firstday.cpp
+++ firstday.cpp
@@ -1,21 +1,22 @@
 #include <iostream>
 
 using namespace std;
-void reverseArray(int intArray[]);
+void reverseArray(int intArray[], int size);
 int main() {
 
-int intArray[5] = {1,2,3,4,5};
-reverseArray(intArray);
+const int size = 4;
+int intArray[size] = {1,2,3,4};
+reverseArray(intArray, size);
   return 0;
 }
 
-void reverseArray(int intArray[]){
+void reverseArray(int intArray[], int size){
 cout << "Normal Array:\n";
-for(int i = 0; i < 5; i++){
+for(int i = 0; i < size; i++){
   cout << intArray[i] << " ";
 }
 cout << "\nReversed Array:\n";
-for(int i = 4; i >= 0; i--){
+for(int i = size - 1; i >= 0; i--){
   cout << intArray[i] << " ";
 }
 cout << "\n";
